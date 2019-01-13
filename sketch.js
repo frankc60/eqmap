605e240191f5b0e90e8a361c2e0d461689192733
frankc60 <frankc60@gmail.com>
2019-01-13 22:36:31 +0100

    got working with NZ geonet data!!

--- sketch.js
+++ sketch.js
@@ -13,10 +13,13 @@ const accessToken = 'pk.eyJ1IjoiZnJhbmtjNjAiLCJhIjoiY2pxdXg1NGN1MDJwMDQycGN4ank5
 let imageMap;
 let earthData;
 
-let zoom = 1;
+let zoom =4;
 
-const clon = 0;
-const clat = 0;
+const clon = 170;
+const clat = -41;
+
+const wdth = 1024;
+const hght = 512
 
 
 let lon = -43.5321;
@@ -24,14 +27,14 @@ let lat = 172.6362;
 
 function preload() {
 //                                               styles/v1/{username}/{style_id}/static/{overlay}/{lon},{lat},{zoom},{bearing},{pitch}|{auto}/{width}x{height}{@2x}
-    imageMap = loadImage("https://api.mapbox.com/styles/v1/mapbox/dark-v10/static/0,0,"+zoom+",0,0/1024x700?access_token=" + accessToken);
+    imageMap = loadImage("https://api.mapbox.com/styles/v1/mapbox/navigation-preview-night-v4/static/"+clon+","+clat+","+zoom+",0,0/"+wdth+"x"+hght+"?access_token=" + accessToken);
     earthData = loadTable("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.csv","csv","header");
-
+    nzData = loadJSON("https://api.geonet.org.nz/quake?MMI=1");
 
 }
 
 function setup() {
-    createCanvas(1024,700);
+    createCanvas(1024,512);
     image(imageMap,0,0)
 
     translate(width/2, height/2); //move top left to center
@@ -43,8 +46,8 @@ function setup() {
     let x = convertX(lat) - cx;
     let y = convertY(lon) - cy;
 
-console.log(x,y)
-
+//console.log(x,y)
+//ellipse(x,y,20,20); //christchurch!!!
 
 
   /*   fill("red")
@@ -52,10 +55,10 @@ console.log(x,y)
     ellipse(1,1,10,10)
  */
     //console.log(earthData.rows[0])
-    let ecl, cl;
+    /* let ecl, cl;
     for(let w=0; w < earthData.rows.length; w++) {
 
-        ecl = map(earthData.rows[w].arr[4],0,10,5,12)
+        ecl = map(earthData.rows[w].arr[4],0,10,4,12)
 
         if(ecl > 9) {
             cl = "red"
@@ -73,7 +76,39 @@ console.log(x,y)
          
         fill(cl)
         ellipse((convertX(earthData.rows[w].arr[2]) - cx), (convertY(earthData.rows[w].arr[1]) - cy), ecl, ecl );
+     
+        // console.log(convertX(earthData.rows[w].arr[2]) - cx)
     }
+ */
+
+    //nz stuff
+    for(let t=0; t < nzData.features.length; t++) {
+
+        xNz = nzData.features[t].geometry.coordinates[0];
+        yNz =nzData.features[t].geometry.coordinates[1];
+
+        magNz = nzData.features[t].properties.magnitude;
+
+        if(magNz > 5) {
+            cl = "red";
+        } else if(magNz > 4) {
+            cl = "orange";
+        } else if(magNz > 3) {
+            cl = "yellow";
+        } else if(magNz > 2) {
+            cl = "blue";
+        } else if(magNz > 1) {
+            cl = "green";
+        } else {
+            cl = "green";
+        }
+
+
+        fill(cl)
+        ellipse(convertX(xNz) - cx, convertY(yNz) - cy, magNz*2, magNz*2 );
+
+    }
+
 }
 
 const convertX = (lon) => {
