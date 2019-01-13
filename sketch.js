7669558f2af6c093e8868468e4fb1e6a24863170
frankc60 <frankc60@gmail.com>
2019-01-13 21:16:41 +0100

    updates

--- sketch.js
+++ sketch.js
@@ -24,7 +24,7 @@ let lat = 172.6362;
 
 function preload() {
 //                                               styles/v1/{username}/{style_id}/static/{overlay}/{lon},{lat},{zoom},{bearing},{pitch}|{auto}/{width}x{height}{@2x}
-    imageMap = loadImage("https://api.mapbox.com/styles/v1/mapbox/dark-v10/static/0,0,"+zoom+",0,0/1024x700?access_token=pk.eyJ1IjoiZnJhbmtjNjAiLCJhIjoiY2pxdXg1NGN1MDJwMDQycGN4ank5dms3ZiJ9.iXaCdWnJD6TQoCF2uxWPdA");
+    imageMap = loadImage("https://api.mapbox.com/styles/v1/mapbox/dark-v10/static/0,0,"+zoom+",0,0/1024x700?access_token=" + accessToken);
     earthData = loadTable("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.csv","csv","header");
 
 
@@ -51,22 +51,18 @@ console.log(x,y)
     ellipse(x,y,10,10)
     ellipse(1,1,10,10)
  */
-    console.log(earthData.rows[0])
-let ecl, cl;
+    //console.log(earthData.rows[0])
+    let ecl, cl;
     for(let w=0; w < earthData.rows.length; w++) {
 
         ecl = map(earthData.rows[w].arr[4],0,10,5,12)
-
-        
 
         if(ecl > 9) {
             cl = "red"
         } else if(ecl > 8) {
             cl = "orange"
-
         } else if(ecl > 7) {
-            cl = "pink"
-
+            cl = "yellow"
         } else if(ecl > 6) {
             cl = "blue"
         } else if(ecl > 5) {
@@ -76,36 +72,11 @@ let ecl, cl;
         }
          
         fill(cl)
-
-        console.log(ecl)
-
         ellipse((convertX(earthData.rows[w].arr[2]) - cx), (convertY(earthData.rows[w].arr[1]) - cy), ecl, ecl );
-
     }
-
-
-/* 
-    let x2 = convertX(earthData.rows[1].arr[1]) - cx;
-    let y2 = convertY(earthData.rows[2].arr[2]) - cy;
-
-
-    ellipse(x2,y2,20,20)
- */
-
-    //arr[4] = magnitude
-
-
-/*
-la t 30.5925     [1]
-long 131.0371   [2]
-mag 6.3          [4]
- */
-
-
-
 }
 
-convertX = (lon) => {
+const convertX = (lon) => {
     lon = radians(lon);
 
     let a = ( 256 / PI ) * pow( 2, zoom );
@@ -115,10 +86,8 @@ convertX = (lon) => {
     return a * b;
 }
 
-convertY = (lat) => {
+const convertY = (lat) => {
     lat = radians(lat);
-
-
 
     let a = ( 256 / PI ) * Math.pow( 2, zoom );
 
@@ -126,7 +95,7 @@ convertY = (lat) => {
 
     let c = PI - Math.log(b);
     
-    console.log(`PI = ${PI}\npow(2,2)=${pow(2,zoom)}\nlat=${lat}\nlog(b) = ${Math.log1p(b)}`)
+//    console.log(`PI = ${PI}\npow(2,2)=${pow(2,zoom)}\nlat=${lat}\nlog(b) = ${Math.log1p(b)}`)
     return a * c;
 }
 
